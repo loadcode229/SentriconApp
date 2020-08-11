7afdd0a0c3a6be1ab1bb1090c3f04f1655c25a0a
loadcode229 <sepone229@gmail.com>
2020-08-10 21:04:33 -0400

    adds necessary routes for stations_controller.rb

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,2 +1,35 @@
 class StationsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    private
+
+    def station_params
+        params.require(:station).permit(
+            :station_number,
+            :activity
+        )
+    end
+
+    def require_login
+        return head(:forbidden) unless session.include? :user_id
+    end
 end
