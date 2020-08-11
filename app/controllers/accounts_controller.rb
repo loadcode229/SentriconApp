e49a6a0270451877b758410bea4b7f92939978db
loadcode229 <sepone229@gmail.com>
2020-08-10 20:56:45 -0400

    adds necessary routes for accounts_controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,29 @@
 class AccountsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    private
+
+    def account_params
+        params.require(:event).permit(
+            :name,
+            :address,
+            :phone_number,
+            :instructions
+        )
+    end
 end
