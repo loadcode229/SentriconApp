8f6a75b409a17ff8d726a7ddfac6ae584a59f2c4
loadcode229 <sepone229@gmail.com>
2020-08-13 20:27:18 -0400

    successfully creating/logging in/logging out a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if @user = User.find_by(name: params[:user][:username])
+        if @user = User.find_by(username: params[:user][:username])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
